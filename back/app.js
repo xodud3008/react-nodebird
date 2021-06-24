0465dd352ba8300dcd3dfab16a2f1d2e6431e740
xodud3008 <xodud3008@gmail.com>
2021-06-24 20:26:32 +0900

    Fix: cors

--- back/app.js
+++ back/app.js
@@ -30,14 +30,19 @@ if(process.env.NODE_ENV === 'production'){
     app.use(morgan('combined'));
     app.use(hpp());
     app.use(helmet());
+    app.use(cors({
+        origin: ['http://sportscocoa.com'],
+        credentials: true,
+    }));
 }else {
     app.use(morgan('dev'));
+    app.use(cors({
+        origin: true,
+        credentials: true,
+    }));
 }
 
-app.use(cors({
-    origin: ['http://localhost:3000', 'sportscocoa.com'],
-    credentials: true,
-}));
+
 
 app.use('/', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
