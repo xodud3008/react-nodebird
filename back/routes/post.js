cad04072234ac6042e0b41e065b9a326b237ea2c
xodud3008 <xodud3008@gmail.com>
2021-06-24 21:47:20 +0900

    Add: multer-s3

--- back/routes/post.js
+++ back/routes/post.js
@@ -2,6 +2,8 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const multerS3 = require('multer-s3');
+const AWS = require('aws-sdk');
 
 const { Post, Image, Comment, User, Hashtag } = require('../models');
 const { isLoggedIn } = require('./middlewares');
@@ -15,16 +17,18 @@ try {
     fs.mkdirSync('uploads');
 }
 
+AWS.config.update({
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    region: 'ap-northeast-2'
+})
 const upload = multer({
-    storage: multer.diskStorage({
-        destination(res, file, done) {
-            done(null, 'uploads');
-        },
-        filename(req, file, done){
-            const ext = path.extname(file.originalname); // 확장자 추출
-            const basename = path.basename(file.originalname, ext)
-            done(null, basename + '_' + new Date().getTime() + ext);
-        },
+    storage: multerS3({
+        s3: new AWS.S3(),
+        bucket: 'react-nodebird-awss3',
+        key(req, file, cb){
+            cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`)
+        }
     }),
     limits: {fileSize: 20 * 1024 * 1024}, //20MB
 });
@@ -80,7 +84,7 @@ router.post('/', isLoggedIn, upload.none(), async (req, res,next) => {
 
 router.post('/images', isLoggedIn, upload.array('image'), async (req, res, next) => {
     console.log(req.files);
-    res.json(req.files.map((v) => v.filename));
+    res.json(req.files.map((v) => v.location));
 });
 
 router.get('/:postId', async (req, res, next) => {
