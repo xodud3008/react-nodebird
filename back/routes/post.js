fdc6c51bfb3db8f04c626aced0c9192fde06da44
xodud3008 <xodud3008@gmail.com>
2021-06-24 22:55:39 +0900

    Add: thumbnail

--- back/routes/post.js
+++ back/routes/post.js
@@ -84,7 +84,7 @@ router.post('/', isLoggedIn, upload.none(), async (req, res,next) => {
 
 router.post('/images', isLoggedIn, upload.array('image'), async (req, res, next) => {
     console.log(req.files);
-    res.json(req.files.map((v) => v.location));
+    res.json(req.files.map((v) => v.location.replace(/\/original\//, '/thumb/')));
 });
 
 router.get('/:postId', async (req, res, next) => {
